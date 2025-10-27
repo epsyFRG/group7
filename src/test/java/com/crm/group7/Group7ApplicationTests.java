47df977792cf540efd9956806e204b0804b52e81
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-10-27 09:41:41 +0100

    Initial commit

---
+++ src/test/java/com/crm/group7/Group7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.group7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
