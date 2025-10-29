ec6313d3a131824d1ad97bc18d030160bddab456
EmanSan9 <sanna950@gmail.com>
2025-10-29 19:37:06 +0100

    Fix ClienteRepo

--- src/main/java/com/crm/group7/repositories/ClienteRepository.java
+++ src/main/java/com/crm/group7/repositories/ClienteRepository.java
@@ -1,22 +1,22 @@
 package com.crm.group7.repositories;
 
 import com.crm.group7.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
-    List<Cliente> findAllByNomeContatto(String nomeContatto);
+public interface ClienteRepository extends JpaRepository<Cliente, UUID>, JpaSpecificationExecutor<Cliente> {
 
-    List<Cliente> findAllByFatturatoAnnualeGreaterThan(double fatturato);
-
-    List<Cliente> findAllByDataInserimentoAfter(LocalDate dataInserimento);
-
-    List<Cliente> findAllByDataUltimoContattoBefore(LocalDate dataUltimoContatto);
-
-
+    @Query("SELECT c FROM Cliente c " +
+            "JOIN c.indirizzi i " +
+            "JOIN i.comune co " +
+            "JOIN co.provincia p " +
+            "WHERE i.tipoIndirizzo = com.crm.group7.entities.enums.TipoIndirizzo.SEDE_LEGALE")
+    Page<Cliente> findAllSortedByProvinciaSedeLegale(Pageable pageable);
 }
